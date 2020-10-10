8e30623f49817d69b3cbbe3dbaf700b0cc6a3649
FelixErnst <felix.gm.ernst@outlook.com>
2020-10-10 11:34:02 +0200

    fixed issue of browser interaction not being available on some systems

--- R/tRNAdbImport-open.R
+++ R/tRNAdbImport-open.R
@@ -22,8 +22,10 @@ NULL
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' open_tdbID("tdbD00000785")
 #' open_mtdbID("mtdbD00000907")
+#' }
 open_tdbID <- function(tdbID, dbURL = TRNA_DB_URL){
   utils::browseURL(paste0(httr::modify_url(dbURL), "DataOutput/Result?ID=",
                           tdbID))
